6f7e138d0ef43a68fe287c4b1ddb750b87305d61
David Riordan <dr@daveriordan.com>
2017-03-01 13:05:17 -0500

    its a package!

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='copyfile',
+      version='0.1',
+      description='copies a file to a location which may not exist',
+      url='http://github.com/riordan/py-copyfile',
+      author='David Riordan',
+      author_email='dr@daveriordan.com',
+      license='Apache-2.0',
+      packages=['copyfile'],
+      zip_safe=False)
